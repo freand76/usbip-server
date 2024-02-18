5262b50bc4d785bacf599a920740d61b05f7effb
Fredrik Andersson <freand@gmail.com>
2024-02-18 12:40:06 +0100

    Add some function docs in api include

--- include/usbip_server_api.h
+++ include/usbip_server_api.h
@@ -18,15 +18,27 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * @brief Callback function type for USB EP0 control packets
+ */
 typedef void(control_data_func_t)(const uint8_t *setup, const uint8_t *data, size_t data_length);
 
+/**
+ * @brief Callback function type for USB EP data packets
+ */
 typedef void(endpoint_data_func_t)(uint8_t ep, const uint8_t *data, size_t data_length);
 
+/**
+ * @brief USB String type used to connect string id with text
+ */
 typedef struct {
         uint8_t id;
         const char *string;
 } usb_string_t;
 
+/**
+ * @brief USBIP Device type containing pointers to descriptors, strings and callbacks
+ */
 typedef struct {
         const uint8_t *device_desc;
         const uint8_t *config_desc;
@@ -36,12 +48,25 @@ typedef struct {
         endpoint_data_func_t *ep_data_callback;
 } usbip_device_t;
 
+/**
+ * @brief Transmit EP data from virtual device to host
+ */
 void usbip_device_transmit(uint8_t ep, const uint8_t *data, size_t data_length);
 
+/**
+ * @brief Start Virtual Device USBIP server
+ */
 bool usbip_server_start(const usbip_device_t *usb_device);
 
+/**
+ * @brief Stop Virtual Device USBIP server
+ */
+void usbip_server_stop(void);
+
+/**
+ * @brief Test if USBIP server has been interrupts (by Ctrl-C)
+ */
 bool usbip_server_interrupted(void);
 
-void usbip_server_stop(void);
 
 #endif
